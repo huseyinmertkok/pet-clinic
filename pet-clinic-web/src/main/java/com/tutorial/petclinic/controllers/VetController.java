35f50eac8ff91cc51075701b09ec820a25a8a001
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-11-06 23:25:00 +0300

    Implementing Vet Index Page and Controller. Closes #1

---
+++ pet-clinic-web/src/main/java/com/tutorial/petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.tutorial.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets/"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
