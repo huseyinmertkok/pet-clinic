ee97ed201fe474e5a283edad5923dda6159e6b4f
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-11-07 01:26:41 +0300

    Implementing Bootstrap for Data. Closes #4

--- pet-clinic-data/src/main/java/com/tutorial/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/tutorial/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.tutorial.petclinic.services.map;
 
 import com.tutorial.petclinic.model.Vet;
-import com.tutorial.petclinic.services.CrudService;
+import com.tutorial.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
