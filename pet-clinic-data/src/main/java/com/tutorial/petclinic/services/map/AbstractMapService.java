9146804dd7ec42d4a0d90384a58a5fd2a2b74f87
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-11-07 02:02:52 +0300

    Update Map Implementation to Manage Setting of ID Property. Closes #13

--- pet-clinic-data/src/main/java/com/tutorial/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/tutorial/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,14 @@
 package com.tutorial.petclinic.services.map;
 
+import com.tutorial.petclinic.model.BaseEntity;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -14,8 +16,15 @@ public abstract class AbstractMapService<T, ID> {
     T findById(ID id){
         return map.get(id);
     }
-    T save(ID id,T object){
-        map.put(id, object);
+    T save(T object){
+        if (object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else{
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
     void delete(T object){
@@ -23,5 +32,9 @@ public abstract class AbstractMapService<T, ID> {
     }
     void deleteById(ID id){
         map.remove(id);
+    }
+
+    private Long getNextId(){
+        return map.keySet().size() + 1L;
     }
 }
