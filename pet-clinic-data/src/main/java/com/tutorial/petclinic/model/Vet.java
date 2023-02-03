b31943acc2a1c390a6b5e9809591ff8cb4e21ce8
Kok, Mert <Mert.Kok@adesso.com.tr>
2023-02-03 09:52:31 +0300

    Update vet with JPA entity mappings. Closes #30

--- pet-clinic-data/src/main/java/com/tutorial/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/tutorial/petclinic/model/Vet.java
@@ -1,8 +1,14 @@
 package com.tutorial.petclinic.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities;
 
     public Set<Speciality> getSpecialities() {
